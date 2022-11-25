a8b12b109e5b341c056500ecba8ca680046ca8a8
Anatol Bogun <anatol@nugob.org>
2022-11-25 17:51:53 +0900

    interpolation makes more sense (wip)

--- src/Levels/smoother.gd
+++ src/Levels/smoother.gd
@@ -1,7 +1,7 @@
 extends Node
 
-var physics_positions := {}
-var previous_physics_positions := {}
+var target_positions := {}
+var origin_positions := {}
 var print = 3
 
 func _process(_delta: float) -> void:
@@ -9,13 +9,13 @@ func _process(_delta: float) -> void:
 #	position = previous_physics_position + velocity * get_physics_process_delta_time() * Engine.get_physics_interpolation_fraction()
 
 	for child in _get_relevant_children():
-		if previous_physics_positions.has(child):
+		if origin_positions.has(child) && target_positions.has(child):
 			# probably also need to filter children by position and velocity properties or handle this differently if they don't exist
-			child.position = previous_physics_positions[child] + child.velocity * get_physics_process_delta_time() * Engine.get_physics_interpolation_fraction()
-#			child.position = previous_physics_positions[child] + child.velocity * get_physics_process_delta_time() * Engine.get_physics_interpolation_fraction() - (physics_positions[child] - previous_physics_positions[child])
+			child.position = target_positions[child] + child.velocity * get_physics_process_delta_time() * Engine.get_physics_interpolation_fraction()
+#			child.position = target_positions[child] + child.velocity * get_physics_process_delta_time() * Engine.get_physics_interpolation_fraction() - (target_positions[child] - origin_positions[child])
 
 			if child.name == "Player" && print > 0:
-				print("position interpolation:     ", child.position)
+				print("position interpolation: ", child.position)
 
 func _physics_process(_delta: float) -> void:
 #   # if the following code was in a Node2D instance this would work in combination with setting the position in _process
@@ -24,23 +24,22 @@ func _physics_process(_delta: float) -> void:
 #	previous_physics_position = position
 
 	for child in _get_relevant_children():
-		if (!physics_positions.has(child)):
-			physics_positions[child] = child.position
-		elif (!previous_physics_positions.has(child)):
-			previous_physics_positions[child] = physics_positions[child]
-			physics_positions[child] = child.position
+		if (!target_positions.has(child)):
+			target_positions[child] = child.position
+		elif (!origin_positions.has(child)):
+			origin_positions[child] = target_positions[child]
+			target_positions[child] = child.position
 		else:
-			physics_positions[child] = child.position
+			origin_positions[child] = target_positions[child]
+			target_positions[child] = child.position
+			child.position = origin_positions[child]
 
 			if child.name == "Player" &&  print > 0:
 				print("+++++ _physics_process +++++")
-				print("position:                   ", child.position)
-				print("physics_positions:          ", physics_positions[child])
-				print("previous_physics_positions: ", previous_physics_positions[child])
+				print("position:               ", child.position)
+				print("origin_positions:       ", origin_positions[child])
+				print("target_positions:       ", target_positions[child])
 				print -= 1
-
-			child.position = previous_physics_positions[child]
-			previous_physics_positions[child] = physics_positions[child]
 
 
 # scene children that ignore any nodes that don't have a _physics_process overwrite
