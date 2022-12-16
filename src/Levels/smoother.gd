7b3d6245faae279c2a0118fb2ffedaae91e2b418
Anatol Bogun <anatol@nugob.org>
2022-12-16 12:05:06 +0900

    cleanup and comments

--- src/Levels/smoother.gd
+++ src/Levels/smoother.gd
@@ -1,22 +1,20 @@
-extends Node
+class_name Smoother extends Node
 
-# NOTE:
-# - This node can only be applied to siblings or (if recursive is true) children or nested children
-#   of this node or its siblings.
+# NOTES:
+# - By default this node applies to its parent, siblings and recursive children. Nodes that have no
+#   cutom _physics_process code or a position property are automatically ignored.
 # - The most common scenario is to make it a direct child of the root node, e.g. in a level scene.
 #   By default it will then smooth all children and deep nested children in the same scene tree.
-# - For this to work this node must be at the top of the scene tree. Hence
-#   get_parent().move_child(self, 0)
-# - For easier understanding consider:
-#	_positions[child][0] is the origin position
-#	_positions[child][1] is the target position
-# - manual alternative that needs to be applied in each physics object:
-#   # _process
-#   position = previous_physics_position + velocity * get_physics_process_delta_time() * Engine.get_physics_interpolation_fraction()
-#   # _physics_process
-#   position = previous_physics_position
-#	move_and_slide()
-#	previous_physics_position = position
+# - If the smoother should be applied to only specific nodes, just select those nodes in the
+#   includes option and disable smooth_parent and recursive, or give each node that should be
+#   smoothed a Smoother node child with the recursive option off.
+# - The code will keep this node as the first child of the parent node because its _physics_process
+#   and _process code must be applied before any other nodes.
+# - When smooth_parent is enabled the process_priority will be kept at a lower (i.e. earlier) value
+#   than the parent's
+# - For easier understanding of the code, consider:
+#	_positions[node][0] is the origin position
+#	_positions[node][1] is the target position
 
 # TO DO:
 # - consider if we need any extra properties such as recursive, includes and excludes (not even sure about the last 2)
@@ -24,7 +22,7 @@ extends Node
 # - may need something similar for rotations, etc. and export vars to include these properties;
 #   need to check what is potentially affected by _physics_process
 
-@export var smooth_parent: = false :
+@export var smooth_parent: = true :
 	set (value):
 		if value == false:
 			# remove parent from _positions in case this gets toggled on and off during runtime
@@ -58,8 +56,6 @@ func reset(nodes) -> void:
 
 
 func _process(_delta: float) -> void:
-#	var physics_interpolation_fraction: = Engine.get_physics_interpolation_fraction()
-
 	for node in _physics_process_nodes:
 		if _positions.has(node) && _positions[node].size() == 2:
 			if _physics_process_just_updated:
@@ -74,9 +70,6 @@ func _physics_process(_delta: float) -> void:
 	var parent: = get_parent()
 	if parent == null: return
 
-	if smooth_parent:
-		process_priority = parent.process_priority - 1
-
 	# move this node to the top of the parent tree (typically a scene's root node) so that it is called before all other _physics_processes
 	parent.move_child(self, 0)
 
@@ -84,7 +77,7 @@ func _physics_process(_delta: float) -> void:
 		process_priority = parent.process_priority - 1
 
 	# it's enough to update the relevant physics process nodes once per _physics_process
-	_physics_process_nodes = _get_physics_process_nodes(parent, !smooth_parent, true)
+	_physics_process_nodes = _get_physics_process_nodes(parent, !smooth_parent)
 
 	for node in _physics_process_nodes:
 		if (!_positions.has(node)):
@@ -105,21 +98,14 @@ func _physics_process(_delta: float) -> void:
 
 
 # get scene children that ignore any nodes that don't have a _physics_process overwrite
-func _get_physics_process_nodes(node: Node, ignoreNode: = false, withIncludes: = false) -> Array[Node]:
+func _get_physics_process_nodes(node: Node, ignoreNode: = false, withIncludes: = true) -> Array[Node]:
 	var nodes: Array[Node] = includes.map( func (include): return get_node_or_null(include)).filter(func (node): return node != null) if withIncludes else []
 
-	# TO DO: this may not be necessary because the smoother node does not work applied to a parent,
-	# so either we save the original parent and move the node under the owner somehow, then call
-	# _get_physics_process_nodes with the original parent (target node), or recursive
-	# only applies to the second node hierarchy down; right now I think the next 2 lines
-	# will do nothing because the root node doesn't validate. However, if the parent node would
-	# validate we would get erratic movements because the _physics_process and _process of the
-	# smoother node being a child of the node to be smoothed gets called after, not before.
 	if !ignoreNode && node != self && !nodes.has(node) && !excludes.has(get_path_to(node)) && node.has_method("_physics_process") && node.get("position"):
 		nodes.push_back(node)
 
 	if recursive:
 		for child in node.get_children():
-			nodes.append_array(_get_physics_process_nodes(child))
+			nodes.append_array(_get_physics_process_nodes(child, false, false))
 
 	return nodes
