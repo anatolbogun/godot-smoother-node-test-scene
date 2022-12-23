94b752ee521f059e878ce8ee5892544e0c598ca9
Anatol Bogun <anatol@nugob.org>
2022-12-23 18:18:09 +0900

    added player screen_exited signal, Smoother check

--- src/Levels/level2d.gd
+++ src/Levels/level2d.gd
@@ -7,24 +7,26 @@ func _ready() -> void:
 
 func _on_node_teleport_started(node: Node) -> void:
 	print("teleporting: ", node.name)
-	$Smoother.reset_node(node)
+	if $Smoother: $Smoother.reset_node(node)
 
 
 func _on_node_screen_entered(node:Node) -> void:
-	# method 1: keep default Smoother settings and exclude off-screen nodes
-	$Smoother.remove_exclude_node(node)
+	if $Smoother:
+		# method 1: keep default Smoother settings and exclude off-screen nodes
+		$Smoother.remove_exclude_node(node)
 
-	# method 2: set Smoother to smooth_parent: false, recursive: false and include on-screen nodes
-#	$Smoother.add_include_node(node)
+		# method 2: set Smoother to smooth_parent: false, recursive: false and include on-screen nodes
+	#	$Smoother.add_include_node(node)
 
-	print("smoothed nodes: ", $Smoother.smoothed_nodes.map(func (node:Node): return node.name))
+		print("smoothed nodes: ", $Smoother.smoothed_nodes.map(func (node:Node): return node.name))
 
 
 func _on_node_screen_exited(node:Node) -> void:
-	# method 1: keep default Smoother settings and exclude off-screen nodes
-	$Smoother.add_exclude_node(node)
+	if $Smoother:
+		# method 1: keep default Smoother settings and exclude off-screen nodes
+		$Smoother.add_exclude_node(node)
 
-	# method 2: set Smoother to smooth_parent: false, recursive: false and include on-screen nodes
-#	$Smoother.remove_include_node(node)
+		# method 2: set Smoother to smooth_parent: false, recursive: false and include on-screen nodes
+	#	$Smoother.remove_include_node(node)
 
-	print("smoothed nodes: ", $Smoother.smoothed_nodes.map(func (node:Node): return node.name))
+		print("smoothed nodes: ", $Smoother.smoothed_nodes.map(func (node:Node): return node.name))
