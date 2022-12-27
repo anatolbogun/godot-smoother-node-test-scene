5573dcd97678416d710ae8934521d542413f28da
Anatol Bogun <anatol@nugob.org>
2022-12-27 18:40:43 +0900

    moved documentation to GitHub

--- src/Classes/smoother.gd
+++ src/Classes/smoother.gd
@@ -1,3 +1,8 @@
+# SMOOTHER NODE
+#
+# For documentation please visit https://github.com/anatolbogun/godot-smoother-node .
+#
+#
 # MIT LICENSE
 #
 # Copyright 2022 Anatol Bogun
@@ -16,77 +21,7 @@
 # NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
 # DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-#
-#
-# SMOOTHER NODE
-# -------------
-#
-# This node interpolates the properties of other nodes between their _physics_process es. The
-# interpolation is applied in the _process loop which ensures that nodes move smoothly, even if the
-# _physics_process is called less often than the games fps rate which is typically synced to the
-# current screen's refresh rate. By default only the node position is interpolated.
-#
-# NOTES:
-# - By default this node applies to its parent, siblings and recursive children. Nodes that have no
-#   custom _physics_process code or are automatically ignored. So are target properties that a node
-#   may not have.
-# - The most common scenario is to make the Smoother a direct child of the root node, e.g. in a
-#   level scene. By default it will then smooth the parent, all children and deep nested children in
-#   the same scene tree.
-# - Any built-in or custom properties can be defined in the Smoother's properties option as long as
-#   they are of a type that can be multiplied with a float, e.g. another float (used for 2d rotation
-#   and skew), Vector2, Vector3, Transform2D, Transform3D, etc.).
-# - By default only the position property is interpolated. Note that at the moment of writing this
-#   class Godot does not display the default ["position"] value for properties in the inspector,
-#   even though the value applies. This may well be fixed in a future Godot release.
-# - If the smoother should be applied to only specific nodes, just select those nodes in the
-#   includes option and disable smooth_parent and recursive, or give each node that should be
-#   smoothed a Smoother node child with the recursive option off.
-# - The excludes option ignores nodes that would otherwise be covered by other Smoother options,
-#   even when the same nodes are listed in includes.
-# - Collision detection still happens in the _physics_process, so if the physics_ticks_per_second
-#   value in the project settings is too low you may experience seemingly incorrect or punishing
-#   collision detection. The default 60 physics_ticks_per_second should a good choice. To test this
-#   node you may want to temporarily reduce physics ticks to a lower value and toggle this node on
-#   and off.
-# - The code will keep this node as the first child of the parent node because its _physics_process
-#   and _process code must run before any other nodes.
-# - When smooth_parent is enabled the process_priority will be kept at a lower value than the
-#   parent's, i.e. it will be processed earlier.
-# - When teleporting a sprite you may want to call reset_node(node) or reset_node_path(path) for the
-#   affected sprite/s, otherwise a teleport (changing the position) may not work as expected.
-# - Performance optimisations: For large levels you may want to optimise things (as you probably
-#   should regardless of using the Smoother node). A good approach would be to use the
-#   VisibleOnScreenNotifier2D to update the includes or excludes array:
-#   1. Add all off-screen moveable nodes to excludes ($Smoother.add_exclude_node(node)) and remove
-#      them when they come on-screen ($Smoother.remove_exclude_node(node)). Since excludes overwrite
-#      all other Smoother options this is the most flexible option. One caveat is that on entering
-#      the tree, the $VisibleOnScreenNotifier2D does not fire the screen_exited signal, so you may
-#      have to emit this in a Node's func _enter_tree via $VisibleOnScreenNotifier2D.is_on_screen().
-#   2. Add all on-screen moveable nodes to includes ($Smoother.add_include_node(node)) and remove
-#      them when they come off-screen ($Smoother.remove_include_node(node)). Since includes adds
-#      nodes but does not interfere with other options you probably should set the smooth_parent and
-#      recursive options to false. On entering the tree, the VisibleOnScreenNotifier2D
-#      automatically fires the screen_entered signal, so nothing needs to be done.
-#   For both methods it's probably a good idea to emit the screen_exited signal on _exit_tree.
-#   You can always check the currently smoothed nodes, e.g.
-#   print("smoothed nodes: ", $Smoother.smoothed_nodes.map(func (node:Node): return node.name))
-# - For easier understanding of the code, consider:
-#	_properties[node][<property name>][0] is the origin value
-#	_properties[node][<property name>][1] is the target value
-#
-# LIMITATIONS:
-# - Currently this does not work with RigidBody2D or RigidBody3D nodes. Please check out
-#   https://github.com/lawnjelly/smoothing-addon/ which has a more complicated setup but has more
-#   precision and less limitations. Or help to make this code work with rigid bodies if it's
-#   possible at all.
-# - Interpolation is one _physics_process step behind because we need to know the origin and target
-#   value for an interpolation to occur, so in a typical scenario this means a delay of 1/60 second
-#   which is the default physics_ticks_per_second in the project settings.
-# - Interpolation does not look ahead for collision detection. That means that if for example a
-#   sprite falls to hit the ground and the last _physics_process step before impact is very close
-#   to the ground, interpolation will still occur on all _physics frames between which may have a
-#   slight impact cushioning effect. However, with 60 physics fps this is hopefully negligible.
+
 
 class_name Smoother extends Node
 
