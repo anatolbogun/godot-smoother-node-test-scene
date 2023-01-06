3d82e0ff339298d52f098252c4b2a796eb8fc17c
Anatol Bogun <anatol@nugob.org>
2023-01-06 12:53:41 +0900

    await frame_post_draw to detect is_on_screen

--- src/Actors/actor.gd
+++ src/Actors/actor.gd
@@ -20,7 +20,12 @@ func _enter_tree() -> void:
 	# For performance improvement via the Smoother.excludes array:
 	# The VisibleOnScreenNotifier2D signal on_screen_entered fires automatically, but not so the
 	# on_screen_exited signal, so we also want to know at this point what nodes are not on screen.
+	# To get an accurate is_on_screen() result we need to await RenderingServer.frame_post_draw, so
+	# the nodes that should be smoothed with this optimisation can only be known on frame 2.
 	# (This can be ignored for performance improvements via the Smoother.includes array.)
+
+	await RenderingServer.frame_post_draw
+#
 	if !$VisibleOnScreenNotifier2D.is_on_screen():
 		_on_screen_exited()
 
